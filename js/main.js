fa8945e9278a4dbcffc0b3add65251df9e1e7c38
mike564 <mhnguyen555@outlook.com>
2022-02-02 11:34:00 -0600

    Added comments and chaged some logs to alerts in JS. Added message in HTML/CSS to open console

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-
+//Random generation of computer's selection of rock, paper, or scissors
 function computerPlay(){
     const rpsArray = ["Rock","Paper","Scissors"];
     let computerSelection = rpsArray[Math.floor(Math.random()*3)]
     return computerSelection;
 }
 
-
+//Prompt for user to enter their selection of rock, paper, or scissors. Checks whether user has typed correctly
 function userPlay(){
     let playerSelection;
+    //Using RegExp to make case insensitive
     while(!(/Rock/i.test(playerSelection) || /Paper/i.test(playerSelection) || /Scissors/i.test(playerSelection))){
         playerSelection = prompt('Enter "Rock", "Paper", or "Scissors": ');
         if(!(/Rock/i.test(playerSelection) || /Paper/i.test(playerSelection) || /Scissors/i.test(playerSelection))){
@@ -17,11 +18,13 @@ function userPlay(){
     return playerSelection;
 }
 
+//Evaluates winner depeinding on user's selection and computer's selection. Returns and prints result
 function playRound(playerSelection,computerSelection){
     console.log(playerSelection);
     console.log(computerSelection);
-
+    //Using RegExp to make case insensitive
     if (/Rock/i.test(playerSelection) && /Scissors/i.test(computerSelection)){
+        //playerSelection[0].toUpperCase() + playerSelection.slice(1,playerSelection.length) makes first letter capitalized at output, regardless of how user entered their selection
         console.log('You win! ' + playerSelection[0].toUpperCase() + playerSelection.slice(1,playerSelection.length) + " beats " + computerSelection + ".");
         return "player wins";
     }
@@ -43,6 +46,7 @@ function playRound(playerSelection,computerSelection){
     }
 }
 
+//Execute the game, calling playRound() five times. Result of each round is returned by playRound() into "result" and adds to player's respective score. Player with highest score wins.
 function game(){
     let round = 1;
     let playerScore = 0;
@@ -63,16 +67,17 @@ function game(){
         console.log("Your Score: " + playerScore +" Computer's Score: " + computerScore)
     }
     if(playerScore>computerScore){
-        console.log("You have won the match!");
+        alert("You have won the match!");
     }
     else if(playerScore<computerScore){
-        console.log("You have lost the match.");
+        alert("You have lost the match.");
     }
     else{
-        console.log("The match has ended in a tie.")
+        alert("The match has ended in a tie.")
     }
 }
 
+//Exceute game at start
 game()
 
 
